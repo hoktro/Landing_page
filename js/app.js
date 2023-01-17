96a287adadeda188b6f5b7e9b2ce9bdfd183b949
hoktro <hoktro18@gmail.com>
2023-01-18 01:27:33 +0700

    Update

--- js/app.js
+++ js/app.js
@@ -20,8 +20,9 @@
 
 /**
  * Define Global Variables
- * 
 */
+
+const sections = document.querySelectorAll("section");
 
 
 /**
@@ -30,6 +31,13 @@
  * 
 */
 
+function isInViewport( currentSection ){
+    let position = currentSection.getBoundingClientRect();
+    return ( 0 <= position.left && 
+            position.right <= ( window.innerWidth || document.documentElement.clientWidth ) &&
+            position.top >= -300 &&
+            position.bottom <= ( 1.3 * window.innerHeight || document.documentElement.clientHeight ) );
+}
 
 
 /**
@@ -39,9 +47,23 @@
 */
 
 // build the nav
+window.addEventListener( 'load', buildNavMenu() );
 
 
 // Add class 'active' to section when near top of viewport
+function activeSection() {
+
+    for( const element of sections ) 
+        if( isInViewport(element) ) {
+            element.classList.add( "your-active-class", "active" );
+            element.querySelector("h2").style.color = "black";
+        }
+        else {
+            element.classList.remove( "your-active-class", "active" );
+            element.querySelector("h2").style.color = "white";
+        }
+
+}
 
 
 // Scroll to anchor ID using scrollTO event
@@ -54,9 +76,63 @@
 */
 
 // Build menu 
+function buildNavMenu() {
+
+    let navbar = document.getElementById('navbar__list');
+
+    for( const element of sections ) {
+        let newItem = document.createElement("li");
+        newItem.classList.add( "navbar__list__item" );
+        newItem.innerHTML = `<a href = "#${element.getAttribute("id")}" class = "menu__link">${element.getAttribute("data-nav")}</a>`;
+        navbar.appendChild( newItem );
+    }
+}
 
 // Scroll to section on link click
+function scrollToSelection() {
+    const navList = document.querySelectorAll(".nav__hyperlink");
+    for( const element of navList ) {
+        element.addEventListener( 'click', function() { 
+            let destination = document.querySelector( element.getAttribute( 'href' ) ).getBoundingClientRect();
+            window.scrollTo( { destination, behavior: 'smooth' } );
+        } )
+    }
+}
 
 // Set sections as active
+window.addEventListener( 'scroll', activeSection ); // active section whenever scroll the screen
 
+// Auto hide navbar
+var myTimeout = setTimeout( hideNavBar, 5000 ); // Auto hide after 5s
+const NavBar = document.querySelector('.navbar__menu');
 
+function hideNavBar() { 
+    NavBar.style.display = "none";
+}
+
+function showNavBar() {
+    NavBar.style.display = "block";
+}
+
+function resetTimeout() {
+    clearTimeout( myTimeout );                      // clear exist timeout
+    showNavBar();                                   // show the navbar
+    myTimeout = setTimeout( hideNavBar, 5000 );     // set new timeout
+}
+
+window.addEventListener( 'scroll', resetTimeout );  // add listener to scroll
+
+// Back to top button
+
+let backButton = document.getElementById('back-to-top');
+let nail = document.querySelector('.main__hero');
+
+window.addEventListener( 'scroll', showBackToTopButton );
+
+function showBackToTopButton() {
+
+    let nailPos = nail.getBoundingClientRect();
+
+    if( nailPos.top <= 0 ) backButton.style.display = "block";
+    else backButton.style.display = "none";
+}
